a5865dcad96ca493471270ba9eaa2277d9ac03f7
ChangtunHan <cyhan1997@163.com>
2018-12-27 23:07:02 +0800

    添加求解数独部分 优化性能

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -10,10 +10,18 @@
 #include <algorithm>
 #include <stack>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
 int map[9][9];
+
+
+/*
+	生成数独终局部分
+*/
+
+
 int FirstRow[8] = { 1,2,3,4,6,7,8,9 };		//学号后两位是13，终局左上角应为5,第一行剩下8个数字
 
 
@@ -28,8 +36,6 @@ void GenerateFirstRow()
 	{
 		break;
 	} 
-	
-
 }
 
 
@@ -119,29 +125,171 @@ void GenerateSudoku(int num)
 		{
 			for (int j = 0; j < 9; j++)
 			{
-				fprintf(fp, "%d", map[i][j]);
 				if (j != 8)
-					fputc(' ', fp);
+					fprintf(fp,"%d ", map[i][j]);
+				else
+					fprintf(fp, "%d", map[i][j]);
 			}
 			fputc('\n', fp);
 
 		}
-		
+		fputc('\n', fp);
 	}
 	fclose(fp);
 }
 
-void SolvingSudoku(char* absolute_path)
-{
+/*
+	求解数独部分
+*/
 
+int N;		//记录需要填数（也就是0）的个数
+int address[81][2];		//记录每个空白的位置（行数和列数
+bool inf[81][9];		//记录每个空白可以填的数
+
+bool Check_2(int Row, int Col)
+{
+	int m = map[Row][Col];
+	for (int i = 0; i < 9; i++)
+	{
+		if (map[i][Col] == 0 || i==Row)
+			continue;
+		if (m == map[i][Col])
+			return false;
+	}
+	for (int i = 0; i < 9; i++)
+	{	
+		if (map[i][Col] == 0 || i == Col)
+			continue;
+		if (m == map[Row][i])
+			return false;
+	}
+	int RowS = Row / 3 * 3;
+	int ColS = Col / 3 * 3;
+	int ColE = ColS + 2;
+	int RowE = RowS + 2;
+	for (int i = RowS; i <= RowE; i++)
+		for (int j = ColS; j <= ColE; j++)
+		{
+			if (i == Row && j == Col)
+				continue;
+			if (map[i][j] == 0)
+				continue;
+			if (m == map[i][j])
+				return false;
+		}
+	return true;
 }
+
+void Pretreatment()
+{
+	N = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (map[i][j] == 0)
+			{
+		
+				address[N][0] = i;
+				address[N][1] = j;
+				N++;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			map[address[i][0]][address[i][1]] = j+1;
+			if (Check_2(address[i][0], address[i][1]))
+				inf[i][j] = true;
+			else inf[i][j] = false;
+		}
+		map[address[i][0]][address[i][1]] = 0;
+	}
+}
+
+bool Solving(int count)
+{
+	if (count < 1)
+		return true;
+	int n = N - count;
+	for (int j = 0; j < 9; j++)
+	{
+		if (inf[n][j])
+		{
+			map[address[n][0]][address[n][1]] = j + 1;
+			if (Check_2(address[n][0], address[n][1]))
+			{
+				if(Solving(count-1))
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+void SolvingSudoku(char path[])
+{
+	FILE *question = fopen(path, "r");
+	FILE *answer = fopen("sudoku.txt", "w+");
+	while (fscanf(question,"%d",&map[0][0])!=EOF)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (i == 0 && j == 0)
+					continue;
+				else {
+					if (!fscanf(question, "%d", &map[i][j]))
+						cout << "读取失败" << endl;
+				}
+			}
+		}
+		Pretreatment();
+		if (!Solving(N))
+			fprintf(answer, "Solving Sudoku failed\n");
+		else {
+			
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+				{
+					if (!Check_2(i, j))
+					{
+						fprintf(answer, "Solving Sudoku failed1\n");
+						return;
+					}
+				}
+			}
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+				{
+					if (j != 8)
+						fprintf(answer, "%d ", map[i][j]);
+					else
+						fprintf(answer, "%d", map[i][j]);
+				}
+				fputc('\n', answer);
+
+			}
+		}
+		fputc('\n', answer);
+	}
+	fclose(answer);
+	fclose(question);
+}
+
 
 
 int main(int argc, char *argv[])
 {
-	clock_t startTime, endTime;
+	/*clock_t startTime, endTime;
 	startTime = clock();
-	srand((unsigned)time(NULL));
+	srand((unsigned)time(NULL));*/
 
 
 
@@ -155,18 +303,41 @@ int main(int argc, char *argv[])
 			if (N == 0)
 				cout << "参数错误" << endl;
 			else
+			{
 				GenerateSudoku(N);
+			}
 		}
 		else {
-			SolvingSudoku(argv[2]);
+			char path[100]={0};
+			int l;
+			l = strlen(argv[2]);
+			int n=0;
+			for (int i = 0; i < l; i++)
+			{
+				if (argv[2][i] == '\\')
+				{
+					path[n++] = '\\';
+					path[n++] = '\\';
+				}
+				else
+				{
+					path[n++] = argv[2][i];
+				}
+
+			}
+			SolvingSudoku(path);
 		}
-		
 	}
+	
+
+	/*char c[] = { "C:\\Users\\韩昌云\\Desktop\\新建文件夹\\problems.txt" };	
+	SolvingSudoku(c);*/
+	/*GenerateSudoku(10000);*/
+
+	/*endTime = clock();
+	cout << (double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;*/
 
 
-
-	endTime = clock();
-	cout << (double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
 }
 
 
